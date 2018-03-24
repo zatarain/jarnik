79f33efc78f904a89d606bc447f7e06b90068b33
Ulises Tirado Zatarain <ulises.tirado@cimat.mx>
2018-03-23 18:19:22 -0700

    Using core functions in POSIX way. Removing environment variables after run a command. Generic help command. Prototype for command new.
    Signed-off-by: Ulises Tirado Zatarain <ulises.tirado@cimat.mx>

--- src/main.sh
+++ src/main.sh
@@ -1,20 +1,26 @@
 #!/bin/sh
+: "
+Jarník Build System [JBS]
+: "
+
+export jarnik=`basename $0`
+export jarnikdir=`dirname $0`/.$jarnik
+. `dirname $0`/.$jarnik/.core
 
 case "$1" in
-	install)
-		command=install;;
-	uninstall)
-		command=uninstall;;
-	upgrade)
-		command=upgrade;;
-	new|-n|/n)
-		command=new;;
-	help|-h|/?|*)
-		command=help;;
+	install) command=install;;
+	uninstall) command=uninstall;;
+	upgrade) command=upgrade;;
+	new|-n|/n) command=new;;
+	help|-h|/?) command=help;;
+	*) command=help;;
 esac
 
 if [ $# -ge 1 ]; then
 	shift
 fi
 
-`dirname $0`/.jarnik/$command "$@" 
+$jarnikdir/$command "$@" 
+
+export jarnik=
+export jarnikdir=
